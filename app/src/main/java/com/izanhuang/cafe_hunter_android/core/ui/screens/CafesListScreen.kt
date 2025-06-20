194f1b013d881685a8318781bb7c8e19c7190373
Izan <60820542+izanhuang@users.noreply.github.com>
2025-06-20 15:19:39 -0400

    Remember list scroll state

--- app/src/main/java/com/izanhuang/cafe_hunter_android/core/ui/screens/CafesListScreen.kt
+++ app/src/main/java/com/izanhuang/cafe_hunter_android/core/ui/screens/CafesListScreen.kt
@@ -2,6 +2,7 @@ package com.izanhuang.cafe_hunter_android.core.ui.screens
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,10 +13,11 @@ import com.izanhuang.cafe_hunter_android.core.ui.components.CafeCard
 
 @Composable
 fun CafesListScreen(cafes: List<PlaceResult>) {
+    val listState = rememberLazyListState()
     var selectedCafe by remember { mutableStateOf<PlaceResult?>(null) }
 
     if (selectedCafe == null) {
-        LazyColumn {
+        LazyColumn(state = listState) {
             items(cafes) { cafe ->
                 CafeCard(place = cafe, onClick = { selectedCafe = cafe })
             }
